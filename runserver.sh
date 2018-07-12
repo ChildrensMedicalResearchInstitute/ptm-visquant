b2a41133411a865b2a4cd7d19bde240e06e39f75
Jonathan Du <jodu8426@uni.sydney.edu.au>
2018-07-12 16:23:49 +1000

    Add bash script to run flask app

---
+++ runserver.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Activate virtual environment
+source activate ptm-mapper
+# Restart server on change
+export FLASK_DEBUG=TRUE
+# Move to this directory and run app
+cd "`dirname "$0"`"
+python3 -m flask run

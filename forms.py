e9d779c9717569e2bf02949df8671e0c4843d4c3
Jonathan Du <jodu8426@uni.sydney.edu.au>
2018-07-20 09:29:27 +1000

    Update form protein field and include validation

--- forms.py
+++ forms.py
@@ -1,9 +1,22 @@
-from wtforms import Form, FileField, StringField, validators
+import requests
+from wtforms import Form, FileField, StringField, validators, ValidationError
+
+class ValidUniProtProtein(object):
+    def __init__(self, message=None):
+        if not message:
+            message = 'Protein "{}" not found in UniProt database.'
+        self.message = message
+
+    def __call__(self, form, field):
+        ENDPOINT = 'http://www.uniprot.org/uniprot/{}.xml'
+        response = requests.get(ENDPOINT.format(field.data))
+        if response.status_code != 200:
+            raise ValidationError(self.message.format(field.data))
 
 class PtmForm(Form):
     accession = StringField(
-        'Protein Family Accession / ID',
-        [validators.InputRequired()]
+        'Protein Entry Name  Accession',
+        [validators.InputRequired(), ValidUniProtProtein()]
     )
     ptm = FileField(
         'Post-translational modifications file',

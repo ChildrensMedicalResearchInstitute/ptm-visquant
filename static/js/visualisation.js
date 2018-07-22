3d1da037e41f1f678d5a015c8dbfd19854bcd5f0
Jonathan Du <jodu8426@uni.sydney.edu.au>
2018-07-22 15:05:11 +1000

    Add markup coordinate labels

--- static/js/visualisation.js
+++ static/js/visualisation.js
@@ -1,6 +1,6 @@
 
 // MAGIC NUMBERS
-var BACKBONE_Y = 50;
+var BACKBONE_Y = 200;
 var BACKBONE_HEIGHT = 8;
 var MOTIF_HEIGHT = BACKBONE_HEIGHT * 2;
 var MOTIF_OPACITY = 0.65;
@@ -8,6 +8,7 @@ var REGION_HEIGHT = BACKBONE_HEIGHT * 3.5;
 var REGION_OPACITY = 0.90;
 var REGION_RECT_RADIUS = 16;
 var MARKUP_HEIGHT = REGION_HEIGHT;
+var MARKUP_Y = BACKBONE_Y - MARKUP_HEIGHT;
 var MARKUP_STROKE_WIDTH = 2;
 
 var PIXELS_PER_AMINO_ACID = 3;
@@ -79,6 +80,40 @@ var markup = svg.selectAll('markup')
   .attr('x1', markup => scaleCoordToCanvas(markup.start))
   .attr('y1', BACKBONE_Y)
   .attr('x2', markup => scaleCoordToCanvas(markup.start))
-  .attr('y2', BACKBONE_Y - MARKUP_HEIGHT)
+  .attr('y2', MARKUP_Y)
   .attr('stroke', markup => markup.lineColour)
   .attr('stroke-width', MARKUP_STROKE_WIDTH);
+
+var markupLabel = svg.selectAll('markup-label')
+  .data(context.markups.filter(
+    markup => markup.display !== false
+  )).enter()
+  .append('text')
+  .text(markup => markup.start)
+  .attr('x', markup => scaleCoordToCanvas(markup.start))
+  .attr('y', MARKUP_Y);
+
+
+// Return true if bounding box 1 intersects bounding box 2, else return false.
+function intersects(bbox1, bbox2) {
+  return !(
+    bbox2.x > bbox1.x + bbox1.width ||
+    bbox2.x + bbox2.width < bbox1.x ||
+    bbox2.y > bbox1.y + bbox1.height ||
+    bbox2.y + bbox2.height < bbox1.y
+  );
+}
+
+// Update label locations to prevent overlap
+markupLabel.sort((a,b) => (a.start - b.start))
+  .each(function() {
+    let that = this;
+    markupLabel.each(function() {
+      let thisBBox = this.getBBox();
+      if (that != this && intersects(thisBBox, that.getBBox())) {
+        let currentHeight = d3.select(that).attr('y');
+        let delta = thisBBox.height * 1.1;
+        d3.select(this).attr('y', +currentHeight - delta);
+      }
+    })
+  });

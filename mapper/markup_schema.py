49cdab935b4d74695a8c552e814bc8246fbf34f5
Jonathan Du <jodu8426@uni.sydney.edu.au>
2018-07-25 19:30:16 +1000

    Clean markup schema file

--- mapper/markup_schema.py
+++ mapper/markup_schema.py
@@ -2,23 +2,6 @@ from marshmallow import fields, Schema, validate
 
 COLOUR_PATTERN = '^#?[0-9a-fA-F]{3,6}$'
 
-def markup_object(mtype, coord):
-    return {
-        "type" : mtype,
-        "start" : coord,
-        "end" : coord,
-        "lineColour" : 'black',
-        "colour" : "#000000",
-        "display" : True,
-        "v_align" : "top",
-        "metadata" : {
-            "type" : mtype,
-            "start" : coord,
-            "end" : coord,
-            "database" : "User defined",
-        },
-    }
-
 class MarkupSchema(Schema):
     markup_type = fields.String(
         required=True,
@@ -42,10 +25,3 @@ class MarkupSchema(Schema):
     v_align = fields.String(validate=[
         validate.OneOf(['top', 'bottom']),
     ])
-
-def string_to_colour(s):
-    h = hash(s)
-    r = (h & 0xFF0000) >> 16
-    g = (h & 0x00FF00) >> 8
-    b = (h & 0x0000FF) >> 0
-    return "#{:02X}{:02X}{:02X}".format(r, g, b)

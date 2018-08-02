164ca3407b62834b08f5c6b13bf05adacc0d0325
Jonathan Du <jodu8426@uni.sydney.edu.au>
2018-08-02 10:45:29 +1000

    Credit translation function to source

--- static/js/visualisation.js
+++ static/js/visualisation.js
@@ -27,6 +27,14 @@ function intersects(r1, r2) {
   );
 }
 
+// https://stackoverflow.com/questions/38224875/replacing-d3-transform-in-d3-v4/38230545#38230545
+function getTranslation(transform) {
+  var g = document.createElementNS("http://www.w3.org/2000/svg", "g");
+  g.setAttributeNS(null, "transform", transform);
+  var matrix = g.transform.baseVal.consolidate().matrix;
+  return [matrix.e, matrix.f];
+}
+
 class Protein {
   constructor(data, config) {
     this.data = data;
@@ -205,24 +213,6 @@ class Protein {
         .attr("width", VALUES_WIDTH)
         .attr("fill", value => scale_chromatic(value));
     });
-
-    function getTranslation(transform) {
-      // Create a dummy g for calculation purposes only. This will never
-      // be appended to the DOM and will be discarded once this function 
-      // returns.
-      var g = document.createElementNS("http://www.w3.org/2000/svg", "g");
-
-      // Set the transform attribute to the provided string value.
-      g.setAttributeNS(null, "transform", transform);
-
-      // consolidate the SVGTransformList containing all transformations
-      // to a single SVGTransform of type SVG_TRANSFORM_MATRIX and get
-      // its SVGMatrix. 
-      var matrix = g.transform.baseVal.consolidate().matrix;
-
-      // As per definition values e and f are the ones for the translation.
-      return [matrix.e, matrix.f];
-    }
 
     // Update heatmap locations to prevent overlap
     heatmap_bars.sort((a, b) => a.start - b.start).each(function () {

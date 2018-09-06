6ac6244b08f5c5d5d628d8528a3800c96e24e73c
Jonathan Du <jdu@cmri.org.au>
2018-09-06 13:41:03 +1000

    Add testcases

--- mapper/test_utils.py
+++ mapper/test_utils.py
@@ -3,23 +3,47 @@ from . import utils
 
 
 class TestUtils(unittest.TestCase):
-    def test_split_accessions(self):
-        testcases = {
-            "BSN_RAT": ["BSN_RAT"],
-            "BSN_RAT ": ["BSN_RAT"],
-            "a,b": ["a", "b"],
-            "a, b": ["a", "b"],
-            "a, b, c, d": ["a", "b", "c", "d"],
-        }
-        for test_in, test_out in testcases.items():
-            self.assertEqual(test_out, utils.split_accessions(test_in))
+    def test_split_accessions_single_value(self):
+        self.assertEqual(["bsn_rat"], utils.split_accessions("bsn_rat"))
+        self.assertEqual(["BSN_RAT"], utils.split_accessions("BSN_RAT"))
 
-    def test_get_protein_domains(self):
-        accessions = ["BSN_RAT", "P88778", "P03311", "P01137"]
+    def test_split_accessions_single_value_with_spaces(self):
+        self.assertEqual(["BSN_RAT"], utils.split_accessions("BSN_RAT "))
+        self.assertEqual(["BSN_RAT"], utils.split_accessions(" BSN_RAT"))
+
+    def test_split_accessions_multiple_values(self):
+        self.assertEqual(["a", "b"], utils.split_accessions("a,b"))
+        self.assertEqual(["a", "b", "c", "d"],
+                         utils.split_accessions("a,b,c,d"))
+
+    def test_split_accessions_multiple_values_with_spaces(self):
+        self.assertEqual(["a", "b"], utils.split_accessions("a, b"))
+        self.assertEqual(["a", "b", "c", "d"],
+                         utils.split_accessions("a, b, c, d"))
+
+    # def test_split_accessions_empty_terms(self):
+    #     self.assertEqual(["a", "b"], utils.split_accessions("a,,b"))
+    #     self.assertEqual(["a", "b"], utils.split_accessions("a,b,"))
+
+    def test_get_protein_domains_by_accession(self):
+        accessions = ["O88778"]
         domain_info = utils.get_protein_domains(accessions)
         for protein in domain_info:
             self.assertTrue(protein)
 
+    def test_get_protein_domains_by_entry_name(self):
+        accessions = ["BSN_RAT"]
+        domain_info = utils.get_protein_domains(accessions)
+        for protein in domain_info:
+            self.assertTrue(protein)
+
+    def test_get_protein_domains_no_graphic_endpoint(self):
+        accessions = ["P88778", "P03311"]
+        domain_info = utils.get_protein_domains(accessions)
+        for protein in domain_info:
+            self.assertTrue(protein)
+
+    def test_get_protein_domains_non_existent_protein(self):
         accessions = ["NOT_RAT", "NOT_ACCESSION"]
         domain_info = utils.get_protein_domains(accessions)
         for protein in domain_info:

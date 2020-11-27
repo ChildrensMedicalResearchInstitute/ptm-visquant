0651d8c7e7faa73f1bc5e2a59b63696676df3908
Jonathan Du <jdu@atlassian.com>
2020-11-27 21:41:00 +1100

    Enhance 404 page

--- visquant/src/layout/PageNotFound.js
+++ visquant/src/layout/PageNotFound.js
@@ -1,5 +1,17 @@
+import { Button, Result } from "antd";
+import { Link } from "react-router-dom";
+
 const PageNotFound = () => (
-    <div>404</div>
+  <Result
+    status="404"
+    title="404"
+    subTitle="Sorry, the page you visited does not exist."
+    extra={
+      <Link to="/">
+        <Button type="default">Back Home</Button>
+      </Link>
+    }
+  />
 )
 
 export default PageNotFound;

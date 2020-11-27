8d407eda0b8f313ff7e0996f8bf38a2a50b10c52
Jonathan Du <jdu@atlassian.com>
2020-11-27 22:17:09 +1100

    Use enum state machine in loading Markdown content

--- visquant/src/layout/ContentFromMarkdown.js
+++ visquant/src/layout/ContentFromMarkdown.js
@@ -1,12 +1,22 @@
 import React from "react";
 import ReactMarkdown from "react-markdown";
 
+import { Result, Button, Skeleton } from 'antd';
+import { Link } from "react-router-dom";
+
+const Status = {
+  PENDING: "pending",
+  REJECTED: "rejected",
+  RESOLVED: "resolved",
+}
+
 class ContentFromMarkdown extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {
       href: props.href,
+      status: Status.PENDING,
       content: null,
     };
   }
@@ -14,17 +24,40 @@ class ContentFromMarkdown extends React.Component {
   componentDidMount() {
     fetch(this.state.href)
       .then(response => response.text())
-      .then(data => this.setState({ content: data }))
-      .catch(error => {
-        this.setState({ content: "Unable to load content." })
-        console.error(error);
+      .then(data => {
+        this.setState({
+          content: data,
+          status: Status.RESOLVED,
+        })
+      })
+      .catch(_ => {
+        this.setState({
+          content: "Unable to load content.",
+          status: Status.REJECTED,
+        })
       });
   }
 
   render() {
-    return (
-      <ReactMarkdown source={this.state.content} />
-    );
+    if (this.state.status === Status.PENDING) {
+      return <Skeleton active />;
+    }
+
+    if (this.state.status === Status.RESOLVED) {
+      return <ReactMarkdown source={this.state.content} />;
+    }
+
+    if (this.state.status === Status.REJECTED) {
+      return <Result
+        status="warning"
+        title="We were unable to load the content."
+        extra={
+          <Link to="/">
+            <Button type="default" key="home">Back Home</Button>
+          </Link>
+        }
+      />
+    }
   }
 }
 

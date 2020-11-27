a9fd15c6b6dff6b5b73b539be97b96588419a87d
Jonathan Du <jdu@atlassian.com>
2020-11-27 22:16:47 +1100

    Key state to enum

--- visquant/src/layout/TopNav.js
+++ visquant/src/layout/TopNav.js
@@ -5,20 +5,22 @@ import { NavLink } from 'react-router-dom';
 
 const { Header } = Layout;
 
+const MenuKey = {
+  MENU_KEY_DRAW: "menu-key-draw",
+  MENU_KEY_HELP: "menu-key-help",
+  MENU_KEY_LICENSE: "menu-key-license",
+  MENU_KEY_GITHUB: "menu-key-github",
+}
+
 class TopNav extends React.Component {
 
-  static MENU_KEY_DRAW = "menu-key-draw";
-  static MENU_KEY_HELP = "menu-key-help";
-  static MENU_KEY_LICENSE = "menu-key-license";
-  static MENU_KEY_GITHUB = "menu-key-github";
-
   state = {
-    currentSelectedKey: TopNav.MENU_KEY_DRAW,
+    currentSelectedKey: MenuKey.MENU_KEY_DRAW,
   };
 
   handleClick = e => {
     console.log(e.key);
-    if (e.key === TopNav.MENU_KEY_GITHUB) {
+    if (e.key === MenuKey.MENU_KEY_GITHUB) {
       return;
     }
     this.setState({ currentSelectedKey: e.key });
@@ -28,16 +30,16 @@ class TopNav extends React.Component {
     return (
       <Header>
         <Menu theme="dark" onClick={this.handleClick} selectedKeys={[this.state.currentSelectedKey]} mode="horizontal">
-          <Menu.Item key={TopNav.MENU_KEY_DRAW} icon={<PushpinOutlined />}>
+          <Menu.Item key={MenuKey.MENU_KEY_DRAW} icon={<PushpinOutlined />}>
             <NavLink to="/">Draw</NavLink>
           </Menu.Item>
-          <Menu.Item key={TopNav.MENU_KEY_HELP} icon={<QuestionCircleOutlined />}>
+          <Menu.Item key={MenuKey.MENU_KEY_HELP} icon={<QuestionCircleOutlined />}>
             <NavLink to="/how-to">Help</NavLink>
           </Menu.Item>
-          <Menu.Item key={TopNav.MENU_KEY_LICENSE} icon={<CopyrightOutlined />}>
+          <Menu.Item key={MenuKey.MENU_KEY_LICENSE} icon={<CopyrightOutlined />}>
             <NavLink to="/license">License</NavLink>
           </Menu.Item>
-          <Menu.Item key={TopNav.MENU_KEY_GITHUB} icon={<GithubOutlined />}>
+          <Menu.Item key={MenuKey.MENU_KEY_GITHUB} icon={<GithubOutlined />}>
             <a href="https://github.com/ChildrensMedicalResearchInstitute/ptm-visquant" target="_blank">GitHub</a>
           </Menu.Item>
         </Menu>

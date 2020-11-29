1ad46edf4ca5d223f41262c37631ccb121e54507
Jonathan Du <jdu@atlassian.com>
2020-11-29 16:04:45 +1100

    Improve construction message

--- visquant/src/content/UnderConstruction.js
+++ visquant/src/content/UnderConstruction.js
@@ -1,18 +1,21 @@
-import { yellow } from "@ant-design/colors";
-import { Card } from "antd";
+import { BuildOutlined } from "@ant-design/icons";
+import { Alert } from "antd";
 import React from "react";
 
 const UnderConstruction = () => (
-  <Card
-    style={{
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
-      height: "60px",
-      backgroundColor: yellow.primary,
-    }}
-  >
-    <p>This page is currently under construction.</p>
-  </Card>
+  <Alert
+    message="Under construction"
+    description={
+      <p>
+        This new PTM Visquant app is under construction and currently lacks some
+        of the existing features in the current prototype. Please check out{" "}
+        <a href="https://visquant.cmri.org.au/">visquant.cmri.org.au</a> if this
+        app is currently missing the feature you need.
+      </p>
+    }
+    type="warning"
+    icon={<BuildOutlined />}
+    showIcon
+  />
 );
 export default UnderConstruction;
